8920df2ffbd12a6e7ac4cc079b271a0b60658362
Rafael <gurbanov.rafael01@gmail.com>
2022-11-04 07:42:17 +0300

    DAPP-4 implement search bar

---
+++ src/main/java/com/cgi/dentistapp/model/SearchArgs.java
@@ -0,0 +1,19 @@
+package com.cgi.dentistapp.model;
+
+/**
+ * Created by Rafael on 04.11.2022.
+ */
+public class SearchArgs {
+    private String value;
+
+    public SearchArgs() {
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String val) {
+        this.value = val;
+    }
+}
